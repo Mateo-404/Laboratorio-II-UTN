ff1ae6acff124495785aad376482d910093381db
TheNineteenn <mateogariboglio@hotmail.com>
2023-09-11 22:50:31 -0300

    Ejercicio 7
    Falta de hacer la clase teclado y la clase Fecha

---
+++ Ejercicio_7/src/main/java/com/mycompany/ejercicio_7/Ejercicio_7.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ejercicio_7;
+import java.util.*;
+import utilidades.Pantalla;
+
+/**
+ *
+ * @author Gary
+ */
+public class Ejercicio_7 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ingresa la cadena: ");
+        Pantalla.imprimirMayusculaCuadrito(scanner.nextLine());
+    }
+}
---
+++ Ejercicio_7/src/main/java/utilidades/Pantalla.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package utilidades;
+
+/**
+ *
+ * @author Gary
+ */
+public class Pantalla {
+    public static void imprimir (String a){
+        System.out.println(a);
+    }
+    public static void imprimirMayuscula(String a){
+        System.out.println(a.toUpperCase());
+    }
+    public static void imprimirMayusculaCuadrito(String a){
+        String cadena, horizontal = " --";
+        int i;
+        for(i=0;i<a.length();i++){
+            horizontal = horizontal + "-";
+        }
+        System.out.println(horizontal + "\n| " + a.toUpperCase() + " |\n" + horizontal);
+    }
+}
